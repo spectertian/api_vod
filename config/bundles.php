5e9eecdc6eb262b0f21ec1410dd0f9cf63b44ebe
zhongsheng <tianzhongsheng168@gmail.com>
2022-08-22 18:15:40 +0800

    monolog

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
